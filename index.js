7efaff7f9c1b46342d7d08333d928977dfc0bf7b
JuanP Pérez <yohanam.paulus@gmail.com>
2020-09-06 23:31:09 +0200

    add ejs as html template manager

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const path = require("path");
 var app = express();
 
 app.set("port", process.env.PORT || 8080);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use("/", (req, res, next) => {
+  if (req.path.endsWith(".html")) {
+    res.render("index", { pagina: req.path });
+  } else {
+    next();
+  }
+});
 
 app.use(express.static("public_html"));
 
