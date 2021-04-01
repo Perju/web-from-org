53b17d1ada9f9d66e197cd8f1f024591be4d20d6
JuanP Pérez <yohanam.paulus@gmail.com>
2021-04-01 19:17:50 +0200

    Btn-scroll-up: Update webserver index.js with btn-scroll script

---
+++ org/scripts/index.js
@@ -0,0 +1,9 @@
+$('document').ready(function () {
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ 'scroll-top' : 0 });
+        __PS_MV_REG = [];
+        return null;
+    });
+    __PS_MV_REG = [];
+    return null;
+});
--- web-server.lisp
+++ web-server.lisp
@@ -27,3 +27,46 @@
 ;; Cambiar la carpeta en la que se monta el servidor
 (setf (acceptor-document-root web-server)
       #p"public_html/")
+
+;;; Scripts front-end
+;; Generar archivo javascript
+(defmacro gen-js-file (url-file ps-func)
+  `(with-open-file (f ,url-file :direction :output :if-exists :supersede)
+     (format f ,ps-func)))
+
+;; Servir archivo javascript
+(defmacro serve-js-file (url-file ps-func)
+  `(define-easy-handler (example :uri ,url-file) ()
+     (setf (content-type*) "text/javascript")
+     ,ps-func))
+
+;; alerta hello world
+(defun greeting-callback ()
+  (ps (alert "Hello World!")))
+
+(defun index-js ()
+  (ps (chain
+       ($ 'document)
+       (ready (lambda ()
+                (chain
+                 ($ ".scroll-up-btn")
+                 (click (lambda ()
+                          (chain
+                           ($ "html")
+                           (animate (create :scroll-top 0)))
+                          NULL)))
+                NULL)))))
+
+;; jquery test
+(defmacro get-by (tag)
+  `(ps (chain ($ ,tag))))
+
+;; (get-by "#mydiv")
+
+(defun run-app ()
+  (start-server web-server)
+  (serve-js-file "/scripts/index.js" (index-js)))
+
+(defun gen-scripts()
+  (gen-js-file "org/scripts/index.js" (index-js)))
+(gen-scripts)
